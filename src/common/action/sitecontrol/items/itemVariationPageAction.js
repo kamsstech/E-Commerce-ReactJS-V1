3cd0674c6c1c5cfff4cd8166a0c422bcf1cc95b4
Marimaju <mari220213@gmail.com>
2021-12-06 21:03:22 +0530

    new back

---
+++ src/common/action/sitecontrol/items/itemVariationPageAction.js
@@ -0,0 +1,46 @@
+// import { REACT_APP_BASE_URL } from "../../../constant/env";
+import { ENV } from "../../../constant/env";
+import { Constants } from "../../../constant/localstorage";
+import axios from "axios";
+import { Types } from "../../../constant/action";
+
+export const itemVariationPageLoading = () => ({
+  type: Types.PAGE_LIST_PAGE_LOADING,
+  payload:[],
+  statuscode:"",
+  loading: true,
+  error: "",
+  msg: ""
+});
+export const itemVariationPageSuccess = (result,code) => ({
+  type: Types.PAGE_LIST_PAGE_SUCCESS,
+  payload: result,
+  statuscode:code,
+  loading: false,
+  error: "",
+  msg: ""
+});
+
+export const itemVariationPageFailure = (errMsg,code) => ({
+  type: Types.PAGE_LIST_PAGE_FAILURE,
+  payload: [],
+  statuscode:code,
+  loading: false,
+  error: errMsg,
+  msg: ""
+});
+
+export const ItemVariationPageAction = () => async (dispatch) => {
+  dispatch(itemVariationPageLoading())
+  // const headers = {
+  //   "Content-Type": "application/json",
+  // };
+  await axios.get(`${ENV.ADMIN_BASE_URL1}/apiaction/curl.php`)
+        .then(response => {
+          // console.log(response)
+      dispatch(itemVariationPageSuccess(response,1))
+   })
+  .catch(error => {
+    dispatch(itemVariationPageFailure("Something went wrong, Please try again later!"));
+  });
+};
